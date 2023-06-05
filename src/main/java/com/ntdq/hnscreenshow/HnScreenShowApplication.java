a101c7addaeda4f458981e500068927ce35ebd02
wjn-jpg <2445158087@qq.com>
2023-06-05 11:20:02 +0800

    海南大屏前端数据展示接口

---
+++ src/main/java/com/ntdq/hnscreenshow/HnScreenShowApplication.java
@@ -0,0 +1,13 @@
+package com.ntdq.hnscreenshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HnScreenShowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HnScreenShowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ntdq/hnscreenshow/HnScreenShowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntdq.hnscreenshow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HnScreenShowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
